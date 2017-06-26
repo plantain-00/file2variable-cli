3c3cb3ee5599ed86386287689c6d244971df1d3a
york yao <yaoao12306@outlook.com>
2017-06-26 18:36:46 +0800

    better import

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,8 +1,10 @@
 declare module "lodash.flatten" {
     function flatten<T>(array: T[][]): T[];
     export = flatten;
+    namespace flatten { }
 }
 declare module "lodash.uniq" {
     function uniq<T>(array: T[]): T[];
     export = uniq;
+    namespace uniq { }
 }

9873f3d69963f70f5c8921c0b806daf92ab913c7
york yao <yaoao12306@outlook.com>
2017-07-13 21:37:22 +0800

    add release script

--- src/index.ts
+++ src/index.ts
@@ -36,78 +36,76 @@ function writeFileAsync(filename: string, data: string) {
     });
 }
 
-export function executeCommandLine() {
-    const argv = minimist(process.argv.slice(2), {
-        "--": true,
-    });
+function printInConsole(message: any) {
+    // tslint:disable-next-line:no-console
+    console.log(message);
+}
+
+export async function executeCommandLine() {
+    const argv = minimist(process.argv.slice(2), { "--": true });
 
     const inputFiles = argv._;
     if (!inputFiles || inputFiles.length === 0) {
-        // tslint:disable-next-line:no-console
-        console.log("Error: no input files.");
-        return;
+        throw new Error("Error: no input files.");
     }
 
     const outputFile: string = argv.o;
     if (!outputFile) {
-        // tslint:disable-next-line:no-console
-        console.log("Error: no output files.");
-        return;
+        throw new Error("Error: no output files.");
     }
 
     const base: string = argv.base;
 
-    Promise.all(inputFiles.map(file => globAsync(file))).then(files => {
-        const uniqFiles = uniq(flatten(files));
+    const files = await Promise.all(inputFiles.map(file => globAsync(file)));
+    const uniqFiles = uniq(flatten(files));
 
-        if (uniqFiles.length === 0) {
-            // tslint:disable-next-line:no-console
-            console.log("Error: no input files.");
-            return;
+    if (uniqFiles.length === 0) {
+        throw new Error("Error: no input files.");
+    }
+
+    const variables: { name: string; value: string; type: "string" | "object" }[] = [];
+
+    for (const file of uniqFiles) {
+        if (!fs.existsSync(file)) {
+            throw new Error(`Error: file: "${file}" not exists.`);
         }
 
-        const variables: { name: string; value: string; type: "string" | "object" }[] = [];
-
-        for (const file of uniqFiles) {
-            if (!fs.existsSync(file)) {
-                // tslint:disable-next-line:no-console
-                console.log(`Error: file: "${file}" not exists.`);
-                return;
-            }
-
-            const variableName = getVariableName(base ? path.relative(base, file) : file);
-            let fileString = fs.readFileSync(file).toString();
-            if (argv["html-minify"] && file.endsWith(".html")) {
-                fileString = minify(fileString, {
-                    collapseWhitespace: true,
-                    caseSensitive: true,
-                    collapseInlineTagWhitespace: true,
-                });
-                variables.push({ name: variableName, value: fileString, type: "string" });
-            } else if (argv.json && file.endsWith(".json")) {
-                variables.push({ name: variableName, value: fileString, type: "object" });
-            } else if (argv.protobuf && file.endsWith(".proto")) {
-                variables.push({ name: variableName, value: JSON.stringify((protobuf.parse(fileString).root as protobuf.Root).toJSON(), null, "    "), type: "object" });
-            } else {
-                variables.push({ name: variableName, value: fileString, type: "string" });
-            }
+        const variableName = getVariableName(base ? path.relative(base, file) : file);
+        let fileString = fs.readFileSync(file).toString();
+        if (argv["html-minify"] && file.endsWith(".html")) {
+            fileString = minify(fileString, {
+                collapseWhitespace: true,
+                caseSensitive: true,
+                collapseInlineTagWhitespace: true,
+            });
+            variables.push({ name: variableName, value: fileString, type: "string" });
+        } else if (argv.json && file.endsWith(".json")) {
+            variables.push({ name: variableName, value: fileString, type: "object" });
+        } else if (argv.protobuf && file.endsWith(".proto")) {
+            variables.push({ name: variableName, value: JSON.stringify((protobuf.parse(fileString).root as protobuf.Root).toJSON(), null, "    "), type: "object" });
+        } else {
+            variables.push({ name: variableName, value: fileString, type: "string" });
         }
+    }
 
-        let target = variables.map(v => {
-            return v.type === "string"
-                ? `export const ${v.name} = \`${v.value}\`;\n`
-                : `export const ${v.name} = ${v.value};\n`;
-        }).join("");
-        if (outputFile.endsWith(".ts") && variables.some(v => v.type === "object")) {
-            target = `// tslint:disable:object-literal-key-quotes trailing-comma
+    let target = variables.map(v => {
+        return v.type === "string"
+            ? `export const ${v.name} = \`${v.value}\`;\n`
+            : `export const ${v.name} = ${v.value};\n`;
+    }).join("");
+    if (outputFile.endsWith(".ts") && variables.some(v => v.type === "object")) {
+        target = `// tslint:disable:object-literal-key-quotes trailing-comma
 ${target}// tslint:enable:object-literal-key-quotes trailing-comma
 `;
-        }
-        writeFileAsync(outputFile, target).then(() => {
-            // tslint:disable-next-line:no-console
-            console.log(`Success: to "${outputFile}".`);
-        });
+    }
+    writeFileAsync(outputFile, target).then(() => {
+        printInConsole(`Success: to "${outputFile}".`);
     });
 }
 
-executeCommandLine();
+try {
+    executeCommandLine();
+} catch (error) {
+    printInConsole(error);
+    process.exit(1);
+}

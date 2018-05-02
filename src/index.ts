c14af49959effdcd3680fed7d8a59e7c3d0ce6b5
york yao <yaoao12306@outlook.com>
2018-05-02 08:51:46 +0800

    refactor: improve code quality

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ import * as chokidar from 'chokidar'
 import * as compiler from 'vue-template-compiler'
 import transpile from 'vue-template-es2015-compiler'
 
-function globAsync (pattern: string, ignore?: string | string[]) {
+function globAsync(pattern: string, ignore?: string | string[]) {
   return new Promise<string[]>((resolve, reject) => {
     glob(pattern, { ignore, nodir: true, dot: true }, (error, matches) => {
       if (error) {
@@ -21,11 +21,11 @@ function globAsync (pattern: string, ignore?: string | string[]) {
   })
 }
 
-function getVariableName (filePath: string) {
+function getVariableName(filePath: string) {
   return camelcase(path.normalize(filePath).replace(/\\|\//g, '-'))
 }
 
-function writeFileAsync (filename: string, data: string) {
+function writeFileAsync(filename: string, data: string) {
   return new Promise<void>((resolve, reject) => {
     fs.writeFile(filename, data, error => {
       if (error) {
@@ -37,36 +37,62 @@ function writeFileAsync (filename: string, data: string) {
   })
 }
 
-async function executeCommandLine () {
+async function executeCommandLine() {
   const argv = minimist(process.argv.slice(2), { '--': true })
 
+  const configData = getConfigData(argv)
+
+  const uniqFiles = await globAsync(configData.files.length === 1 ? configData.files[0] : `{${configData.files.join(',')}}`)
+  if (uniqFiles.length === 0) {
+    throw new Error('Error: no input files.')
+  }
+
+  const watchMode: boolean = argv.w || argv.watch
+
+  if (watchMode) {
+    watchFileChanges(configData, uniqFiles)
+  } else if (uniqFiles.length > 0) {
+    const variables = await Promise.all(uniqFiles.map(file => fileToVariable(file, configData.out, configData.base, configData.handler(file))))
+    writeVariables(variables, configData.out)
+  }
+}
+
+// tslint:disable-next-line:cognitive-complexity
+function watchFileChanges(configData: ConfigData, uniqFiles: string[]) {
+  const variables: Variable[] = []
+  let count = 0
+  chokidar.watch(configData.files).on('all', (type: string, file: string) => {
+    console.log(`Detecting ${type}: ${file}`)
+    const handler = configData.handler(file)
+    if (type === 'add' || type === 'change') {
+      const index = variables.findIndex(v => v.file === file)
+      fileToVariable(file, configData.out, configData.base, handler).then(variable => {
+        if (index === -1) {
+          variables.push(variable)
+        } else {
+          variables[index] = variable
+        }
+        count++
+        if (count >= uniqFiles.length) {
+          writeVariables(variables, configData.out)
+        }
+      })
+    } else if (type === 'unlink') {
+      const index = variables.findIndex(v => v.file === file)
+      if (index !== -1) {
+        variables.splice(index, 1)
+        writeVariables(variables, configData.out)
+      }
+    }
+  })
+}
+
+function getConfigData(argv: minimist.ParsedArgs): ConfigData {
   let configData: ConfigData
   if (argv.config) {
     configData = require(path.resolve(process.cwd(), argv.config))
   } else {
-    configData = {
-      base: argv.base,
-      files: argv._,
-      handler: file => {
-        if (file.endsWith('.html')) {
-          if (argv.vue) {
-            return { type: 'vue', name: argv['vue-type-name'], path: argv['vue-type-path'] }
-          }
-          if (argv['html-minify']) {
-            return { type: 'html-minify' }
-          }
-          return { type: 'text' }
-        }
-        if (file.endsWith('.json') && argv.json) {
-          return { type: 'json' }
-        }
-        if (file.endsWith('.proto') && argv.protobuf) {
-          return { type: 'protobuf' }
-        }
-        return { type: 'text' }
-      },
-      out: argv.o
-    }
+    configData = getDefaultConfigData(argv)
   }
 
   if (!configData.files || configData.files.length === 0) {
@@ -77,53 +103,40 @@ async function executeCommandLine () {
     throw new Error('Error: no output files.')
   }
 
-  globAsync(configData.files.length === 1 ? configData.files[0] : `{${configData.files.join(',')}}`).then(uniqFiles => {
-    if (uniqFiles.length === 0) {
-      throw new Error('Error: no input files.')
-    }
-
-    const watchMode: boolean = argv.w || argv.watch
-
-    if (watchMode) {
-      const variables: Variable[] = []
-      let count = 0
-      chokidar.watch(configData.files).on('all', (type: string, file: string) => {
-        console.log(`Detecting ${type}: ${file}`)
-        const handler = configData.handler(file)
-        if (type === 'add' || type === 'change') {
-          const index = variables.findIndex(v => v.file === file)
-          fileToVariable(file, configData.out, configData.base, handler).then(variable => {
-            if (index === -1) {
-              variables.push(variable)
-            } else {
-              variables[index] = variable
-            }
-            count++
-            if (count >= uniqFiles.length) {
-              writeVariables(variables, configData.out)
-            }
-          })
-        } else if (type === 'unlink') {
-          const index = variables.findIndex(v => v.file === file)
-          if (index !== -1) {
-            variables.splice(index, 1)
-            writeVariables(variables, configData.out)
-          }
-        }
-      })
-    } else if (uniqFiles.length > 0) {
-      Promise.all(uniqFiles.map(file => fileToVariable(file, configData.out, configData.base, configData.handler(file)))).then(variables => {
-        writeVariables(variables, configData.out)
-      })
-    }
-  })
+  return configData
 }
 
-function escapeLiteralString (value: string) {
+function getDefaultConfigData(argv: minimist.ParsedArgs): ConfigData {
+  return {
+    base: argv.base,
+    files: argv._,
+    handler: file => {
+      if (file.endsWith('.html')) {
+        if (argv.vue) {
+          return { type: 'vue', name: argv['vue-type-name'], path: argv['vue-type-path'] }
+        }
+        if (argv[htmlMinifyName]) {
+          return { type: htmlMinifyName }
+        }
+        return { type: 'text' }
+      }
+      if (file.endsWith('.json') && argv.json) {
+        return { type: 'json' }
+      }
+      if (file.endsWith('.proto') && argv.protobuf) {
+        return { type: 'protobuf' }
+      }
+      return { type: 'text' }
+    },
+    out: argv.o
+  }
+}
+
+function escapeLiteralString(value: string) {
   return value.replace(/`/g, '\\`').replace(/\$/g, '\\$')
 }
 
-function getExpression (variable: Variable, isTs: boolean) {
+function getExpression(variable: Variable, isTs: boolean) {
   if (variable.type === 'string') {
     return `export const ${variable.name} = \`${escapeLiteralString(variable.value)}\`\n`
   }
@@ -149,33 +162,12 @@ export ${staticResult}
   }
 }
 
-function writeVariables (variables: Variable[], out: string) {
+function writeVariables(variables: Variable[], out: string) {
   variables.sort((v1, v2) => v1.name.localeCompare(v2.name))
   let target: string
   if (out.endsWith('.ts')) {
     target = variables.map(v => getExpression(v, true)).join('')
-    const handlerNameSet = new Set<string>()
-    const handlerNameMap: { [path: string]: string[] } = {}
-    for (const variable of variables) {
-      if (variable.handler.type === 'vue' && variable.handler.name && variable.handler.path) {
-        // Foo<any> -> Foo
-        const handlerName = variable.handler.name.indexOf('<') !== -1
-          ? variable.handler.name.substring(0, variable.handler.name.indexOf('<'))
-          : variable.handler.name
-        if (!handlerNameSet.has(handlerName)) {
-          handlerNameSet.add(handlerName)
-          if (!handlerNameMap[variable.handler.path]) {
-            handlerNameMap[variable.handler.path] = []
-          }
-          handlerNameMap[variable.handler.path].push(handlerName)
-        }
-      }
-    }
-    const handlerPaths = Object.keys(handlerNameMap).sort((h1, h2) => h1.localeCompare(h2))
-    const vueTypesImport = handlerPaths.map(handlerPath => {
-      const handlerNames = handlerNameMap[handlerPath].sort((n1, n2) => n1.localeCompare(n2)).join(', ')
-      return `import { ${handlerNames} } from "${handlerPath}"\n`
-    }).join('')
+    const vueTypesImport = getVueTypesImport(variables)
     target = `// tslint:disable
 ${vueTypesImport}
 ${target}// tslint:enable
@@ -196,63 +188,100 @@ ${target}`
   })
 }
 
-function fileToVariable (file: string, out: string, base: string | undefined, handler: Handler) {
+function getVueTypesImport(variables: Variable[]) {
+  const handlerNameSet = new Set<string>()
+  const handlerNameMap: { [path: string]: string[] } = {}
+  for (const variable of variables) {
+    if (variable.handler.type === 'vue' && variable.handler.name && variable.handler.path) {
+      // Foo<any> -> Foo
+      const handlerName = variable.handler.name.indexOf('<') !== -1
+        ? variable.handler.name.substring(0, variable.handler.name.indexOf('<'))
+        : variable.handler.name
+      if (!handlerNameSet.has(handlerName)) {
+        handlerNameSet.add(handlerName)
+        if (!handlerNameMap[variable.handler.path]) {
+          handlerNameMap[variable.handler.path] = []
+        }
+        handlerNameMap[variable.handler.path].push(handlerName)
+      }
+    }
+  }
+  const handlerPaths = Object.keys(handlerNameMap).sort((h1, h2) => h1.localeCompare(h2))
+  return handlerPaths.map(handlerPath => {
+    const handlerNames = handlerNameMap[handlerPath].sort((n1, n2) => n1.localeCompare(n2)).join(', ')
+    return `import { ${handlerNames} } from "${handlerPath}"\n`
+  }).join('')
+}
+
+function fileToVariable(file: string, out: string, base: string | undefined, handler: Handler) {
   return new Promise<Variable>((resolve, reject) => {
     const variableName = getVariableName(base ? path.relative(base, file) : file)
     fs.readFile(file, (error, data) => {
       if (error) {
         reject(error)
       } else {
-        let fileString = data.toString()
-        if (handler.type === 'vue') {
-          resolve({
-            name: variableName,
-            file,
-            value: fileString,
-            handler,
-            type: 'function'
-          })
-        } else if (handler.type === 'html-minify') {
-          fileString = minify(fileString, {
-            collapseWhitespace: true,
-            caseSensitive: true,
-            collapseInlineTagWhitespace: true
-          })
-          resolve({
-            name: variableName,
-            file,
-            value: fileString,
-            handler,
-            type: 'string'
-          })
-        } else if (handler.type === 'json') {
-          resolve({
-            name: variableName,
-            file,
-            value: JSON.stringify(JSON.parse(fileString), null, out.endsWith('.ts') ? 4 : 2),
-            handler,
-            type: 'object'
-          })
-        } else if (handler.type === 'protobuf') {
-          resolve({
-            name: variableName,
-            file,
-            value: JSON.stringify((protobuf.parse(fileString).root as protobuf.Root).toJSON(), null, out.endsWith('.ts') ? 4 : 2),
-            handler,
-            type: 'object'
-          })
-        } else {
-          resolve({
-            name: variableName,
-            file,
-            value: fileString,
-            handler,
-            type: 'string'
-          })
-        }
+        const variable = getVariable(handler, variableName, file, data, out)
+        resolve(variable)
       }
     })
   })
+}
+
+const htmlMinifyName = 'html-minify'
+
+function getVariable(
+  handler: Handler,
+  variableName: string,
+  file: string,
+  data: Buffer,
+  out: string): Variable {
+  let fileString = data.toString()
+  if (handler.type === 'vue') {
+    return {
+      name: variableName,
+      file,
+      value: fileString,
+      handler,
+      type: 'function'
+    }
+  } else if (handler.type === htmlMinifyName) {
+    fileString = minify(fileString, {
+      collapseWhitespace: true,
+      caseSensitive: true,
+      collapseInlineTagWhitespace: true
+    })
+    return {
+      name: variableName,
+      file,
+      value: fileString,
+      handler,
+      type: 'string'
+    }
+  } else if (handler.type === 'json') {
+    return {
+      name: variableName,
+      file,
+      value: JSON.stringify(JSON.parse(fileString), null, out.endsWith('.ts') ? 4 : 2),
+      handler,
+      type: 'object'
+    }
+  } else if (handler.type === 'protobuf') {
+    return {
+      name: variableName,
+      file,
+      value: JSON.stringify(protobuf.parse(fileString).root.toJSON(), null, out.endsWith('.ts') ? 4 : 2),
+      handler,
+      type: 'object'
+    }
+  } else {
+    return {
+      name: variableName,
+      file,
+      value: fileString,
+      handler,
+      type: 'string'
+    }
+  }
 }
 
 type Variable = {

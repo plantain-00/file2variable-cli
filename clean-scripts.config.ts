b4682954353d6e17480c3adaf12987889eaf4127
York Yao <me@yorkyao.com>
2020-05-16 21:17:00 +0800

    fix: types path

--- clean-scripts.config.ts
+++ clean-scripts.config.ts
@@ -1,13 +1,13 @@
-const { checkGitStatus } = require('clean-scripts')
+import { checkGitStatus } from 'clean-scripts'
 
-const tsFiles = `"src/**/*.ts" "spec/**/*.ts"`
+const tsFiles = `"src/**/*.ts"`
 const jsFiles = `"*.config.js"`
 
 const tscSrcCommand = `tsc -p src`
 const demoCommand = 'node dist/index.js --config demo/file2variable1.config.ts'
 const demo2Command = 'node dist/index.js --config demo/file2variable2.config.ts'
 
-module.exports = {
+export default {
   build: [
     `rimraf dist/`,
     tscSrcCommand,
@@ -22,9 +22,7 @@ module.exports = {
     typeCoverage: 'type-coverage -p src --strict'
   },
   test: [
-    'tsc -p spec',
-    'jasmine',
-    'clean-release --config clean-run.config.js',
+    'clean-release --config clean-run.config.ts',
     () => checkGitStatus()
   ],
   fix: `eslint --ext .js,.ts ${tsFiles} ${jsFiles} --fix`,

079c13f449ed8170508403898b3daa5db278e095
York Yao <me@yorkyao.com>
2020-09-22 20:44:26 +0800

    refactor: remove unused code

--- src/lib.d.ts
+++ src/lib.d.ts
@@ -1,16 +1,3 @@
-declare module 'vue-template-compiler' {
-  function compile(template: string): {
-    render: string;
-    staticRenderFns: string[];
-  }
-}
-
-declare module 'vue-template-es2015-compiler' {
-  function transpile(template: string): string
-
-  export = transpile
-}
-
 declare module '*.json' {
   export const version: string
 }
